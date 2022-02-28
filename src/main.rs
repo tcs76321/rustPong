d019e83625326cb2f03c7c3011a547a34e5e58d0
Trevor Stahl <trevorclarkstahl@gmail.com>
2022-02-28 12:15:58 -0700

    rust is frustrating

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use std::io::{stdin,stdout,Write};
+use std::io::prelude::*;
 
 fn main() {
-    let app = Application::builder().application_id("rustPong").build();
+    println!();
 
-    app.connect_activate(|app| {
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(240)
-            .default_height(240)
-            .title("Pong in Rust")
-            .build();
-        window.show_all();
-    });
 
-    app.run();
+    startMenu();
+}
+
+fn startMenu() {
+    let mut looping: bool = true;
+    let mut input = String::new();
+
+    while looping {
+        println!("==== Pong in Rust, on the terminal, because dependencies are crazy ====");
+        println!("==== ---------------------- By Trevor Stahl ---------------------- ====");
+        println!("!!! -Please adjust your terminal screen so you only see this menu - !!!");
+        println!("-------- that is, 'Pong in Rust ...' should be at the top -------------");
+        println!("-----------------------------------------------------------------------");
+        println!("-----------------------------------------------------------------------");
+        println!("----- and 'BOTTOM' should be at the bottom, just above your input -----");
+        println!("-----------------------------------------------------------------------");
+        println!("------ to loop this and test, enter anything other than 'done' --------");
+        println!("-----------------------------------------------------------------------");
+        println!("--------------------- once set up, enter 'done' -----------------------");
+        println!("------------------------------ BOTTOM ---------------------------------");
+        let _=stdout().flush();
+        stdin().read_line(&mut input);
+        if input == "done\n" {
+            looping = false;
+        }
+    }
 }
