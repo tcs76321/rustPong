abeeec55ae6240fdc1876dff9df3bfc7db363852
Trevor Stahl <trevorclarkstahl@gmail.com>
2022-02-27 21:59:32 -0700

    maybe not, not yet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("\n");
-    println!("");
+    let app = Application::builder().application_id("rustPong").build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(240)
+            .default_height(240)
+            .title("Pong in Rust")
+            .build();
+        window.show_all();
+    });
+
+    app.run();
 }
